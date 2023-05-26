765c6cd47b96c65c072120488f77ecc99564d1b4
colopasquali <130797622+colopasquali@users.noreply.github.com>
2023-05-26 03:20:17 -0300

    javascript
    variables

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var nombre = "Franco";
+var apellido = "Pasquali";
+console.log(nombre + " " + apellido);
+
+let numeroUno = 10 
+let numeroDos = 15
+let suma = numeroUno + numeroDos
+console.log(suma);
+let numeroTres = suma + 20
+console.log(numeroTres);
+
